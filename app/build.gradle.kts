42551fb25f7589d256c095bc5cf54b9f837c3590
Bruno Felix <bruno.felix@fuze.cc>
2023-07-07 00:10:07 -0300

    :heavy_plus_sign: Novas dependências adicionadas no Gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,12 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-android")
+    id("com.google.devtools.ksp")
+    id("kotlin-parcelize")
+    id("com.google.dagger.hilt.android")
+    id("androidx.navigation.safeargs.kotlin")
+    id("kotlin-kapt")
 }
 
 android {
@@ -30,18 +36,87 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
     kotlinOptions {
         jvmTarget = "17"
+    }
+
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
     }
 }
 
 dependencies {
+    // Kotlin(Core
+    implementation("androidx.core:core-ktx:1.10.1")
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    // Activity / Fragment KTX for viewModels()
+    implementation("androidx.activity:activity-ktx:1.7.2")
+    implementation("androidx.fragment:fragment-ktx:1.6.0")
+
+    // AppCompat
     implementation("androidx.appcompat:appcompat:1.6.1")
+
+    // Material Design
     implementation("com.google.android.material:material:1.9.0")
+
+    // ConstraintLayout
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // Timber
+    implementation("com.jakewharton.timber:timber:5.0.1")
+
+    // Gson
+    implementation("com.google.code.gson:gson:2.9.0")
+
+    // Preferences
+    implementation("androidx.preference:preference-ktx:1.2.0")
+
+    // Splash API
+    implementation("androidx.core:core-splashscreen:1.0.1")
+
+    // Architecture Component
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+
+    // Lifecycle
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-common-java8:2.6.1")
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+
+    // Glide
+    implementation("com.github.bumptech.glide:glide:4.15.1")
+
+    // Navigation Component
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
+
+    // Dagger Hilt
+    implementation("com.google.dagger:hilt-android:2.46.1")
+    kapt("com.google.dagger:hilt-compiler:2.46.1")
+
+    // Room
+    implementation("androidx.room:room-runtime:2.5.2")
+    implementation("androidx.room:room-ktx:2.5.2")
+    implementation("androidx.room:room-paging:2.5.2")
+    ksp("androidx.room:room-compiler:2.5.2")
+
+    // Paging
+    implementation("androidx.paging:paging-runtime-ktx:3.1.1")
+
+    // Unit Tests
     testImplementation("junit:junit:4.13.2")
+
+    // Instrumented / UI Tests
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+kapt {
+    correctErrorTypes = true
 }
